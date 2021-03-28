790f6dfc42edbf87ea110388feb4ff22d5aa2bc8
jorisre <reixjoris@gmail.com>
2021-03-28 15:33:49 +0200

    docs(example): use vitejs

--- example/src/components/NotSupported.tsx
+++ example/src/components/NotSupported.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const NotSupported = () => (
   <span className="support">
     <svg
--- example/src/components/Switch.tsx
+++ example/src/components/Switch.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 interface Props {
   disabled?: boolean;
   checked?: boolean;
